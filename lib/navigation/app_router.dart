76aa7b82f51cd007fa212aa11b6fea62a586eeb5
arthurpels <pipsiopus@gmail.com>
2026-04-08 20:45:20 +0700

    fix few bags

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -132,7 +132,7 @@ GoRouter createRouter(AuthCubit authCubit) {
         builder: (_, __, navigationShell) => MultiBlocProvider(
           providers: [
             BlocProvider.value(value: getIt<FavoritesCubit>()),
-            BlocProvider.value(value: getIt<ProfileCubit>()),
+            BlocProvider.value(value: getIt<ProfileCubit>(instanceName: 'myProfile')),
           ],
           child: ScaffoldWithBottomNav(navigationShell: navigationShell),
         ),
